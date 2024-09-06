b72a9ba7cba3a42616bf1c437e2b21bc7b529043
andersbor <anbo@zealand.dk>
2024-09-06 10:59:09 +0200

    error message on missing input

--- app/src/main/java/com/example/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/MainActivity.kt
@@ -14,14 +14,17 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
@@ -30,19 +33,25 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.shoppinglist.ui.theme.ShoppingListTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class) // TopAppBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ShoppingListTheme {
                 val items = remember { mutableStateListOf<String>() }
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = { TopAppBar(title = { Text("Shopping list") }) }
+                )
+                { innerPadding ->
                     ShoppingList(
                         items = items,
                         onAddItem = { items.add(it) },
@@ -67,17 +76,32 @@ fun ShoppingList(
     var newItem by remember { mutableStateOf("") }
     Column(modifier = modifier.padding(10.dp)) {
         Row(verticalAlignment = CenterVertically) {
+            var inputError by remember { mutableStateOf(false) }
             OutlinedTextField(
                 modifier = Modifier.weight(1f),
                 value = newItem,
                 label = { Text("New Item") },
                 onValueChange = { newItem = it },
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                supportingText = {
+                    if (inputError)
+                        Text("This field is required", color = Color.Red)
+                }
             )
             Button(
-                onClick = { onAddItem(newItem) },
-                modifier = Modifier.padding(start = 5.dp)
-            ) { Text("Add") }
+                onClick = {
+                    if (newItem.isBlank()) {
+                        inputError = true
+                    } else {
+                        inputError = false
+                        onAddItem(newItem)
+                        newItem = ""
+                    }
+                },
+                modifier = Modifier.padding(start = 5.dp),
+            ) {
+                Icon(Icons.Outlined.Add, contentDescription = "Add")
+            }
         }
         LazyColumn {
             items(items) { item ->
